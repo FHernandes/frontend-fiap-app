e53df6401c036de8d85675583034f1a92c9c2ef6
FHernandes <fernandabeltramehernandes@gmail.com>
2025-05-29 11:34:23 -0300

    wip: add images, style and some utils

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
+'use client';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Intro from './components/Intro';
+import Water from './components/Water';
+import Cursos from './components/Cursos';
+import Faq from './components/Faq';
+import { useState } from 'react';
+
 export default function Home() {
+  const [stage, setStage] = useState<string>('header');
+
   return (
     <>
-      <Navbar />
-      <div>
-        <p>Header</p>
-        <p>Intro</p>
-        <p>Água</p>
-        <p>Cursos</p>
-        <p>FAQ</p>
-      </div>
+      <Navbar stage={stage} />
+      <Header setStage={setStage} />
+      <Intro setStage={setStage} />
+      <Water setStage={setStage} />
+      <Cursos setStage={setStage} />
+      <Faq setStage={setStage} />
     </>
   );
 }
---
+++ src/app/utils/coursesData.ts
@@ -0,0 +1,37 @@
+const courseData = [
+  {
+    category: 'tecnologia',
+    title: 'Tecnologia',
+    items: [
+      { name: 'Big Data Ecosystem', description: 'remoto • live' },
+      { name: 'Creating Dashboards for BI', description: 'remoto • live' },
+      {
+        name: 'Big Data Science - Machine Learning & Data Mining',
+        description: 'remoto • live + multimídia',
+      },
+      { name: 'Storytelling', description: 'remoto • live' },
+    ],
+  },
+  {
+    category: 'inovacao',
+    title: 'Inovação',
+    items: [
+      { name: 'UX', description: 'DIGITAL' },
+      { name: 'UX Writing', description: 'live' },
+      { name: 'Storytelling para Negócios', description: 'live' },
+      { name: 'Chatbots', description: 'live' },
+    ],
+  },
+  {
+    category: 'negocios',
+    title: 'Negócios',
+    items: [
+      { name: 'Agile Culture', description: 'digital' },
+      { name: 'DPO Data Protection Officer', description: 'live' },
+      { name: 'IT Business Partner', description: 'live' },
+      { name: 'Perícia Forense Computacional', description: 'live' },
+    ],
+  },
+];
+
+export default courseData;
