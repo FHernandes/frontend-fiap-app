0fea4f970f5487209084e420d17baba1a5b5d43c
FHernandes <fernandabeltramehernandes@gmail.com>
2025-05-29 12:00:32 -0300

    fix: fix behavior for mobile

---
+++ src/app/utils/coursesFaqData.ts
@@ -0,0 +1,31 @@
+export const CoursesFaqData = [
+  {
+    title: 'QUANDO POSSO ME MATRICULAR?',
+    description:
+      'Você pode se matricular em qualquer dia e hora, basta acessar a página do curso e se inscrever.',
+  },
+  {
+    title: 'POSSO FAZER DOIS OU MAIS CURSOS AO MESMO TEMPO?',
+    description:
+      'Sim. Apenas atente-se às datas, elas devem ser diferentes, porque cada curso tem sua dinâmica.',
+  },
+  {
+    title: 'QUAIS OS PRÉ-REQUISITOS?',
+    description:
+      'Cada curso tem seus pré-requisitos descritos na própria página. Identifique-os, para que você obtenha um melhor aproveitamento do seu SHIFT.',
+  },
+  {
+    title: 'QUAL A DURAÇÃO DOS CURSOS?',
+    description: 'De 6 a 42 horas.',
+  },
+  {
+    title: 'PRECISO LEVAR ALGUM MATERIAL PARA AS AULAS?',
+    description:
+      'Não. Os materiais utilizados em sala de aula são fornecidos pela FIAP e as aulas mais técnicas são realizadas em nossos próprios laboratórios. Sugerimos somente que traga o que preferir para suas anotações.',
+  },
+  {
+    title: 'VOU RECEBER CERTIFICADO DE CONCLUSÃO DE CURSO?',
+    description:
+      'Sim. Ao cumprir pelo menos 75% da carga horária do curso, você receberá um Certificado Digital, que poderá ser acessado na plataforma.',
+  },
+];
