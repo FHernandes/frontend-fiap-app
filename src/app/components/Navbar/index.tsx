2137a9f8a72f2778a5c89feba6ea1f0e5114fa1d
FHernandes <fernandabeltramehernandes@gmail.com>
2025-05-29 11:31:42 -0300

    wip: add progress bar

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,11 +1,44 @@
+'use client';
+import { useEffect } from 'react';
 import Image from 'next/image';
-import logo from '../../assets/images/logo-fiap.svg';
+import gsap from 'gsap';
+
+import logo from '@/app/assets/images/logo-fiap.svg';
 import style from './navbar.module.scss';
 
-const Navbar = () => (
-  <nav className={style.navbar}>
-    <Image src={logo} alt="FIAP" className={style.logo} priority />
-  </nav>
-);
+interface NavbarProps {
+  stage: string;
+}
+
+const stageMap: Record<string, number> = {
+  header: 0,
+  intro: 1,
+  water: 2,
+  cursos: 3,
+  faq: 4,
+};
+
+const Navbar = ({ stage }: NavbarProps) => {
+  useEffect(() => {
+    const step = stageMap[stage] ?? 0;
+
+    gsap.to(`.${style.progressBarFill}`, {
+      width: `${(step / 4) * 100}%`,
+      duration: 0.5,
+      ease: 'power2.out',
+    });
+  }, [stage]);
+
+  return (
+    <div className={style.container}>
+      <nav className={style.navbar}>
+        <Image src={logo} alt="FIAP" className={style.logo} priority />
+      </nav>
+      <div className={style.progressBar}>
+        <div className={style.progressBarFill}></div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
