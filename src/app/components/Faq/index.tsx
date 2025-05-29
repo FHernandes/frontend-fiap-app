14b2d22b69755c0b0acf736230e2713a4bbd0c4b
FHernandes <fernandabeltramehernandes@gmail.com>
2025-05-29 11:59:58 -0300

    fix: fix behavior for mobile

--- src/app/components/Faq/index.tsx
+++ src/app/components/Faq/index.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
+import { CoursesFaqData } from '@/app/utils/coursesFaqData';
 import style from './faq.module.scss';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -16,6 +17,7 @@ interface FaqProps {
 const Faq = ({ setStage }: FaqProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const itemsRef = useRef<Array<HTMLDivElement | null>>([]);
+  const [openItem, setOpenItem] = useState<number[]>([]);
 
   useGSAP(() => {
     ScrollTrigger.create({
@@ -56,7 +58,7 @@ const Faq = ({ setStage }: FaqProps) => {
 
       item.addEventListener('mouseleave', () => {
         gsap.to(line, {
-          width: '40px', // ou valor inicial que você quiser
+          width: '40px',
           duration: 0.5,
           ease: 'power2.inOut',
         });
@@ -64,37 +66,11 @@ const Faq = ({ setStage }: FaqProps) => {
     });
   }, []);
 
-  const courses = [
-    {
-      title: 'QUANDO POSSO ME MATRICULAR?',
-      description:
-        'Você pode se matricular em qualquer dia e hora, basta acessar a página do curso e se inscrever.',
-    },
-    {
-      title: 'POSSO FAZER DOIS OU MAIS CURSOS AO MESMO TEMPO?',
-      description:
-        'Sim. Apenas atente-se às datas, elas devem ser diferentes, porque cada curso tem sua dinâmica.',
-    },
-    {
-      title: 'QUAIS OS PRÉ-REQUISITOS?',
-      description:
-        'Cada curso tem seus pré-requisitos descritos na própria página. Identifique-os, para que você obtenha um melhor aproveitamento do seu SHIFT.',
-    },
-    {
-      title: 'QUAL A DURAÇÃO DOS CURSOS?',
-      description: 'De 6 a 42 horas.',
-    },
-    {
-      title: 'PRECISO LEVAR ALGUM MATERIAL PARA AS AULAS?',
-      description:
-        'Não. Os materiais utilizados em sala de aula são fornecidos pela FIAP e as aulas mais técnicas são realizadas em nossos próprios laboratórios. Sugerimos somente que traga o que preferir para suas anotações.',
-    },
-    {
-      title: 'VOU RECEBER CERTIFICADO DE CONCLUSÃO DE CURSO?',
-      description:
-        'Sim. Ao cumprir pelo menos 75% da carga horária do curso, você receberá um Certificado Digital, que poderá ser acessado na plataforma.',
-    },
-  ];
+  const toggleItem = (index: number) => {
+    setOpenItem((prev) =>
+      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index],
+    );
+  };
 
   return (
     <div className={style.container} ref={containerRef}>
@@ -105,11 +81,14 @@ const Faq = ({ setStage }: FaqProps) => {
         </div>
 
         <div className={style.courses}>
-          {courses.map((course, i) => (
+          {CoursesFaqData.map((course, i) => (
             <div
               key={i}
-              className={style.courseContainer}
+              className={`${style.courseContainer} ${
+                openItem.includes(i) ? style.open : ''
+              }`}
               ref={(el) => (itemsRef.current[i] = el)}
+              onClick={() => toggleItem(i)}
             >
               <div className={style.line} />
               <p className={style.title2}>{course.title}</p>
